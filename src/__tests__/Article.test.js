a1db4909ab0e8bb27c2a79d747f0b20a15923537
Immanuel Kinuthia <immanuel.kinuthia@student.moringaschool.com>
2025-07-09 11:14:12 +0300

    Running Tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,14 @@ import React from "react";
 
 import Article from "../components/Article";
 
+
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
+
 });
